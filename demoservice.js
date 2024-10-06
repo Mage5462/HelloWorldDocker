ab40b152ebb1d618eb45e70e42902d245947d74f
Mage5462 <mageshwariperiasamy5@gmail.com>
2024-10-06 21:35:49 +0530

    Update demoservice.js

--- demoservice.js
+++ demoservice.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var bodyParser=require('body-parser')
 var app=express()
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 var mysql = require('mysql')
